94d1e699684676e79023781ce3b9038e55ad7f5c
Hugo Flores Perez <hugofloresperez@Hugos-MacBook-Pro.local>
2020-07-27 12:10:40 -0400

    Base project

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Hugo Flores Perez on 7/27/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
