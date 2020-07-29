c1d6ad243ba07df67c4ef52c06078b8b1f65db9f
Hugo Flores Perez <hugofloresperez@Hugos-MacBook-Pro.local>
2020-07-29 13:52:58 -0400

    Basic view implemented

--- UberClone/ViewController.swift
+++ UberClone/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import MapKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "Pick location"
+        label.backgroundColor = .green
+        return label
+    }()
+    
+    let mapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setUpLayout()
     }
-
-
+    
+    func setUpLayout() {
+        setUpTitle()
+        setUpMap()
+    }
+    
+    func setUpTitle() {
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.layoutMarginsGuide)
+            make.leading.equalTo(view)
+            make.trailing.equalTo(view)
+        }
+    }
+    
+    func setUpMap() {
+        view.addSubview(mapView)
+        mapView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom)
+            make.leading.equalTo(view)
+            make.trailing.equalTo(view)
+            make.bottom.equalTo(view.layoutMarginsGuide)
+        }
+    }
 }
-
